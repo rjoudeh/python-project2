76c7cb045fca89636a2eb855c8a33d6bd29629d7
Rania Joudeh <rjoudeh@expedia.com>
2022-09-21 00:46:14 +0300

    Add extra fields

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = 'postgresql://yynfpfcmzaurur:86559a5c68397a1e1c13caa3da96a0176517227e2673043e0e13c8ec04775a05@ec2-3-219-19-205.compute-1.amazonaws.com:5432/da104r99sjglr0'
-
+# 'sqlite:///User.sqlite3'
 db = SQLAlchemy(app)
 
 
@@ -17,11 +17,15 @@ class User(db.Model):
     # Makes three columns into the table id, name, email
     _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
 
     # A constructor function where we will pass the name and email of a user and it gets add as a new entry in the table.
-    def __init__(self, name, email):
+    def __init__(self, name, address, age, email):
         self.name = name
+        self.address = address
+        self.age = age
         self.email = email
 
 
@@ -36,9 +40,11 @@ def index():
     if request.method == 'POST':  # When a user clicks submit button it will come here.
         data = request.form  # request the data from the form in index.html file
         name = data["name"]
+        address = data["address"]
+        age = data["age"]
         email = data["email"]
 
-        new_data = User(name, email)
+        new_data = User(name, email, address, age)
         db.session.add(new_data)
         db.session.commit()
 
@@ -46,7 +52,8 @@ def index():
 
         return render_template("index.html", user_data=user_data)  # passes user_data variable into the index.html file.
 
-    return render_template("index.html")
+    user_data = User.query.all()
+    return render_template("index.html", user_data=user_data)
 
 
 if __name__ == "__main__":
